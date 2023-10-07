633222881a1b4b604088a961c436584ef991ab30
Siddhant Baviskar <siddhantbaviskar@outlook.com>
2023-10-07 23:54:23 +0530

    PHP Insights Auto Fix

--- repository/laravel/app/Exceptions/Handler.php
+++ repository/laravel/app/Exceptions/Handler.php
@@ -2,8 +2,8 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
@@ -23,7 +23,7 @@ class Handler extends ExceptionHandler
      */
     public function register(): void
     {
-        $this->reportable(function (Throwable $e) {
+        $this->reportable(function (Throwable $e): void {
             //
         });
     }
