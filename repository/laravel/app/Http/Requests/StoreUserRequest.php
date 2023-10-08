726f2f1caf27b0b95b8ae0af022184ecec2f3556
Siddhant Baviskar <siddhantbaviskar@outlook.com>
2023-10-08 09:55:37 +0530

    TDD user creation without first name test pass

--- repository/laravel/app/Http/Requests/StoreUserRequest.php
+++ repository/laravel/app/Http/Requests/StoreUserRequest.php
@@ -2,15 +2,20 @@
 
 namespace App\Http\Requests;
 
+use App\Http\Resources\FailedValidationResource;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Validation\Rules\Password;
+use Illuminate\Validation\ValidationException;
 
 class StoreUserRequest extends FormRequest
 {
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     * @return array<string, ValidationRule|array|string>
      */
     public function rules(): array
     {
@@ -24,5 +29,26 @@ class StoreUserRequest extends FormRequest
             'avatar' => ['nullable', 'string', 'exists:files,uuid'],
             'is_marketing' => ['nullable', 'boolean'],
         ];
+    }
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param Validator $validator
+     * @return void
+     *
+     * @throws ValidationException
+     */
+    protected function failedValidation(Validator $validator): void
+    {
+        $response = response()->json([
+            'success' => 0,
+            'data' => [],
+            'error' => 'Failed Validation',
+            'errors' => $validator->errors(),
+            'trace' => [],
+        ], 422);
+
+        throw new ValidationException($validator, $response);
     }
 }
