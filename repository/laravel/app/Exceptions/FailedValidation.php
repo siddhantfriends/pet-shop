510c2245ec99cee20f5dd9053cf9c7ced48644e5
Siddhant Baviskar <siddhantbaviskar@outlook.com>
2023-10-09 09:20:54 +0530

    Larastan Bug Fix

--- repository/laravel/app/Exceptions/FailedValidation.php
+++ repository/laravel/app/Exceptions/FailedValidation.php
@@ -3,10 +3,24 @@
 namespace App\Exceptions;
 
 use Exception;
+use JetBrains\PhpStorm\Pure;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Validation\ValidationException;
 
 class FailedValidation extends Exception
 {
+    public ValidationException $ex;
+
+    /**
+     * FailedValidation constructor.
+     */
+    #[Pure] public function __construct(string $message, int $code, ValidationException $previous)
+    {
+        parent::__construct($message, $code, $previous);
+
+        $this->ex = $previous;
+    }
+
     /**
      * Report the exception.
      */
@@ -20,6 +34,6 @@ class FailedValidation extends Exception
      */
     public function render(): JsonResponse
     {
-        return JsonResponse::error($this->getMessage(), $this->getPrevious()->validator->errors(), $this->getCode());
+        return JsonResponse::error($this->getMessage(), $this->ex->validator->errors(), $this->getCode());
     }
 }
