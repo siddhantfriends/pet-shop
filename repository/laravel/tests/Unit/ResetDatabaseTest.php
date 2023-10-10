444571117198eaa84694032e7e369b7c2426770b
Siddhant Baviskar <siddhantbaviskar@outlook.com>
2023-10-10 22:13:16 +0530

    Test can execute reset database

---
+++ repository/laravel/tests/Unit/ResetDatabaseTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ResetDatabaseTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * Tests the app reset database command
+     * @test
+     */
+    public function can_execute_reset_database(): void
+    {
+        $this->artisan('app:reset-database')
+            ->assertSuccessful();
+
+        $admin = 1;
+        $totalUsers = $admin + config('pet-shop.seeder.users');
+
+        $this->assertDatabaseCount(User::class, $totalUsers);
+    }
+}
