fde2df9c5ead2fd3f9cd265fd072edbd8f9c8a1b
Siddhant Baviskar <siddhantbaviskar@outlook.com>
2023-10-13 21:02:48 +0530

    OrderFactory and Order Model

---
+++ repository/laravel/app/Models/Order.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use App\Http\Service\GenerateUUIDService;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+/**
+ * @template T
+ *
+ * @extends BaseModel<T>
+ */
+class Order extends BaseModel
+{
+    use HasFactory;
+
+    protected static function boot(): void
+    {
+        parent::boot();
+
+        app(GenerateUUIDService::class)->handle(new Order());
+    }
+
+    public function getRouteKeyName(): string
+    {
+        return 'uuid';
+    }
+}
